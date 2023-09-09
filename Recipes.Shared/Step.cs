e82eab32daefeff038920f0f22dea074f39e3161
arojas <alejandro.rojas@talent.com>
2023-09-08 19:29:55 -0500

    Project base - initial commit

---
+++ Recipes.Shared/Step.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recipes.Shared
+{
+    internal class Step
+    {
+    }
+}
