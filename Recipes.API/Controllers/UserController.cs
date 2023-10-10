0d40010795cee34224c4deda5527f3cddbb96b92
ELIANA MARCELA RESTREPO DUQUE <elianamarcelar199@gmail.com>
2023-10-09 22:54:13 -0500

    Controllers creation

---
+++ Recipes.API/Controllers/UserController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Recipes.API.Data;
+using Recipes.Shared.Entities;
+
+namespace Recipes.API.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UserController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.users.ToListAsync());
+        }
+
+
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var user = await _context.users.FirstOrDefaultAsync(x => x.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+
+        // Post- Create
+        [HttpPost]
+        public async Task<ActionResult> Post(User user)
+        {
+
+            _context.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok(user);
+        }
+
+        // Put-- update
+        [HttpPut]
+        public async Task<ActionResult> Put(User user)
+        {
+
+            _context.Update(user);
+            await _context.SaveChangesAsync();
+            return Ok(user);
+        }
+
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+
+            var filaAfectada = await _context.users
+                .Where(x => x.Id == id)
+                .ExecuteDeleteAsync();
+
+            if (filaAfectada == 0)
+            {
+
+                return NotFound();
+
+            }
+
+            return NoContent();
+        }
+
+    }
+}
